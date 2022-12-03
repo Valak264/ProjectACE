5223b5b2d54bb4bedf73b301eb196e641ec30d96
Valak264 <gaelicius@gmail.com>
2022-12-03 15:53:38 +0700

    Sudah bisa scan file dengan normal

--- bot2.py
+++ bot2.py
@@ -1,9 +1,9 @@
-import vt, requests, discord, os, time
+import vt, requests, discord, time, os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_SERVER')
 VT_APIKEY = os.getenv('VT_API_KEY')
 discord_client = discord.Client()
@@ -25,16 +25,20 @@ async def scanning_file():
         analysis = await client.scan_file_async(f)
         f.close()
         if f.closed:
-            print("File closed !")
+            if os.path.exists("temp_file"):
+                os.remove("temp_file")
+            print("File closed and deleted ! Wait for analysis status")
         else:
-            print("Error ! File not closing !")
+            print("Error ! File not found !")
             exit()
     
     while True:
-        obj = client.get_object("/analyses/{}", analysis.id)
+        obj = await client.get_object_async("/analyses/{}", analysis.id)
         print("Analysis status : ", obj.status)
         if obj.status == "completed":
             print("Stats : ", obj.stats)
+            await client.close_async()
+            return obj
         time.sleep(30)
 
 @discord_client.event
@@ -44,3 +48,14 @@ async def on_ready():
         f'{discord_client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+@discord_client.event
+async def on_message(message):
+    attachment = message.attachments
+    url = str(attachment[0])
+    await download_file(url)
+    scan = await scanning_file()
+    print(scan.stats)
+    exit()
+
+discord_client.run(TOKEN)
