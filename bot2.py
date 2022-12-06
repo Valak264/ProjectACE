16eb2e6dfd40300a1cebbca3f192f672c22db0e5
Valak264 <gaelicius@gmail.com>
2022-12-06 17:05:02 +0700

    File Scanner fixed

--- bot2.py
+++ bot2.py
@@ -1,4 +1,4 @@
-import vt, requests, discord, time, os
+import vt, requests, discord, os, json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,7 +6,12 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_SERVER')
 VT_APIKEY = os.getenv('VT_API_KEY')
-discord_client = discord.Client()
+
+intent = discord.Intents.default()
+intent.message_content = True
+intent.messages = True
+
+discord_client = discord.Client(intents=intent)
 
 async def download_file(url):
     response = requests.get(url)
@@ -14,9 +19,9 @@ async def download_file(url):
     f.write(response.content)
     f.close()
     if f.closed:
-        print("File closed !")
+        print("File telah ditutup !")
     else:
-        print("Error ! File not closing !")
+        print("Error ! File tidak bisa ditutup !")
         exit()
 
 async def scanning_file():
@@ -27,35 +32,40 @@ async def scanning_file():
         if f.closed:
             if os.path.exists("temp_file"):
                 os.remove("temp_file")
-            print("File closed and deleted ! Wait for analysis status")
+            print("File telah ditutup dan dihapus ! Mohon tunggu sampai ")
         else:
-            print("Error ! File not found !")
+            print("Error ! File tidak ditemukan !")
             exit()
     
     while True:
         obj = await client.get_object_async("/analyses/{}", analysis.id)
-        print("Analysis status : ", obj.status)
+        # print("Analysis status : ", obj.status)
         if obj.status == "completed":
-            print("Stats : ", obj.stats)
+            print("Status Analisa : ", obj.status)
             await client.close_async()
             return obj
-        time.sleep(30)
-
+        
 @discord_client.event
 async def on_ready():
     guild = discord.utils.get(discord_client.guilds, name=SERVER)
     print(
-        f'{discord_client.user} is connected to the following guild:\n'
+        f'{discord_client.user} terhubung dengan server berikut:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
 @discord_client.event
 async def on_message(message):
     attachment = message.attachments
-    url = str(attachment[0])
-    await download_file(url)
-    scan = await scanning_file()
-    print(scan.stats)
-    exit()
+    if len(attachment) == 1:
+        url = str(attachment[0])
+        await download_file(url)
+        scan = await scanning_file()
+        str_scan = str(scan.stats)
+        convert = str_scan.replace("'",'"')
+        json_format = json.loads(convert)
+        if json_format["malicious"] >= 4:
+            print("File Berbahaya")
+        else:
+            print("File Aman")
 
 discord_client.run(TOKEN)
